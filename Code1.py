62d271874c5eb63b4474316d85c201c7e8972682
Code22 <sergeawahm@yahoo.com>
2023-05-24 09:00:39 +0100

    code to determine if the entered name has an even or odd number of characters

--- Code1.py
+++ Code1.py
@@ -1,1 +1,7 @@
-
+n=input('Enter your name')
+count=int(len(n))
+if(count%2==0):
+    print('The name you entered has an even number of charaters')
+else:
+    print('The name you entered has an odd number of charaters')
+    
